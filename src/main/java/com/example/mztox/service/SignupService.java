216c15565b9f5930eea0c0ab6067082cffd960de
seokholee <sukho8407@gmail.com>
2024-07-16 16:39:48 +0900

    mztox

--- src/main/java/com/example/mztox/service/SignupService.java
+++ src/main/java/com/example/mztox/service/SignupService.java
@@ -17,16 +17,14 @@ public class SignupService {
     //DTO(Data Transfer Object)와 엔티티(Entity) 간의 변환을 쉽게 하기 위해 사용되는 라이브러리
     private final ModelMapper modelMapper;
 
-    public String signup(SignupDto signupDto) {
+    public void signup(SignupDto signupDto) {
 
-        if (memberRepository.findByEmail(signupDto.getEmail()).isPresent()) {
-            return "똑같은 이메일이 있습니다.";
-        }
+
             Members member = modelMapper.map(signupDto, Members.class);
             member.setPassword(passwordEncoder.encode(signupDto.getPassword()));
             memberRepository.save(member);
 
-            return ;
+
         }
     }
 
